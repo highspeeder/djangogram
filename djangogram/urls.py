f21bf555f32749dbccda69e26d6eae79ca33e5c2
Joshua <asdf3765@naver.com>
2023-07-23 15:24:40 +0900

    feed생성 기능 추가(이미지 업로드, 피드데이터 DB insert)

--- djangogram/urls.py
+++ djangogram/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from content.views import Main
+from content.views import Main, UploadFeed
+from django.conf.urls.static import static
+from .settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("main/", Main.as_view())
+    path("main/", Main.as_view()),
+    path('content/upload/', UploadFeed.as_view()),
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
---
+++ static/js/main.js
@@ -0,0 +1,135 @@
+let files;
+
+function updateCounter(el) {
+    $('#test_cnt').html($(el).val().length + "/2200");
+
+    // 글자수 제한
+    if ($(el).val().length > 2200) {
+        $(el).val($(el).val().substring(0, 2200));
+        $('#test_cnt').html("2200 / 2200");
+    };
+};
+
+function second_modal_back() {
+    $('#first_modal').css({ "display": "flex" });
+    $('#second_modal').css({ "display": "none" });
+    $('#upload_icon').css({ "color": "rgb(0, 0, 0)" });
+    $('#first_modal_image_upload').css({ "background-color": "transparent" });
+}
+
+function close_modal_event(el) {
+    document
+        .querySelector(".second_modal_window")
+        .classList
+        .toggle("open");
+    $(el)
+        .parent()
+        .css({ 'display': 'none' });
+    $(document.body).css({ overflow: "auto" });
+}
+
+$('#sidenav_addbox_button').click(function () {
+    $('#first_modal').css({
+        "top": window.scrollY + "px",
+        "display": "flex"
+    });
+    $(document.body).css({ overflow: "hidden" });
+});
+
+$('#first_modal')
+    .on("dragover", dragOver)
+    .on("dragleave", dragleave)
+    .on("drop", uploadFiles);
+
+function dragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.type == "dragover") {
+        $('#upload_icon').css({ "color": "rgb(0, 149, 246)" });
+        $('#first_modal_image_upload').css({ "background-color": "rgb(245, 245, 245)" });
+    }
+
+}
+
+function dragleave(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.type == "dragleave") {
+        $('#upload_icon').css({ "color": "rgb(0, 0, 0)" });
+        $('#first_modal_image_upload').css({ "background-color": "transparent" });
+    }
+}
+
+function uploadFiles(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer = e.originalEvent.dataTransfer;
+    files = e.target.files || e.dataTransfer.files;
+    console.log('파일이 올라옴.' + files[0].name);
+    if (files.length > 1) {
+        alert('파일은 한개만 올려주세요.');
+        return;
+    }
+    if (files[0].type.match(/image.*/)) {
+        $('.modal_image_upload').css({
+            "background-image": "url(" + window
+                .URL
+                .createObjectURL(files[0]) + ")",
+            "outline": "none",
+            "background-size": "cover",
+            "background-position": "center"
+        });
+        $('#second_modal_image_upload').css({
+            "background-image": "url(" + window
+                .URL
+                .createObjectURL(files[0]) + ")",
+            "outline": "none",
+            "background-size": "cover",
+            "background-position": "center"
+        });
+        $('#first_modal').css({ display: "none" });
+        $('#second_modal').css({
+            top: window.scrollY + "px",
+            display: "flex"
+        });
+        document.querySelector('.second_modal_window').classList.toggle("open");
+    } else {
+        alert('이미지가 아닙니다.');
+        return;
+    }
+}
+
+$('#feed_share_button').click(function () {
+    let file = files[0];
+    let image = files[0].name;
+    let content = $('#share_textbox').val();
+    let user_id = 'dummy';
+    let profile_image = 'https://images.unsplash.com/photo-1688530324360-33002e7f9736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80';
+
+    let formdata = new FormData();
+    formdata.append('file', file);
+    formdata.append('image', image);
+    formdata.append('content', content);
+    formdata.append('user_id', user_id);
+    formdata.append('profile_image', profile_image);
+
+    $.ajax({
+        url: '/content/upload/',
+        data: formdata,
+        method: 'POST',
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            console.log('성공');
+        },
+        error: function (request, status, error) {
+            console.log('에러');
+        },
+        complete: function () {
+            console.log('완료');
+            location.replace('/main');
+        }
+    })
+
+
+});
